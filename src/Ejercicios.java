909793fd5a015d277a1e8b7c0a616fed563a43fe
Turupawn <turupawn@userr>
2014-06-16 07:05:07 -0600

    primer commit

---
+++ src/Ejercicios.java
@@ -0,0 +1,56 @@
+
+public class Ejercicios
+{	
+	//devuelve true si numero es multiplo de 3, de lo contrario devuelve false
+	//2 puntos
+	static boolean esMultiploDeTres(int num)
+	{
+		return false;
+	}
+	
+	//devuelve el numero menor entre a, b o c
+	//3 puntos
+	static int getMenor(int a, int b, int c)
+	{
+		return -1;
+	}
+	
+	//devuelve el promedio los elementos de un arreglo
+	//5 puntos
+	static int getPromedio(int arreglo[])
+	{
+		return -1;
+	}
+	
+	//Realizar una funcion con las siguientes indicaciones:
+	//Si el parametro recibido es "Honduras" devolvera "Tegucigalpa"
+	//Si el parametro recibido es "Francia" devolvera "Paris"
+	//Si el parametro recibido es "Inglaterra" devolvera "Londres"
+	static String getCapital(String pais)
+	{
+		return "";
+	}
+	
+	//realizarla siguiente funcion recurisva:
+	
+	/* Pseudocodigo*/
+	//funcionRecursiva(num)
+	//  si num == 0
+	//    devolver 0
+	//  fin si
+	//  devolver num + funcionRecursiva(num-1)
+	
+	/*Notacion matematica*/
+	//funcionRecusriva(num) = num + funcionRecursiva(num-1)
+	//Donde funcionRecursiva(0) = 0
+	
+	//1 punto oro extra
+	static int funcionRecursiva(int num)
+	{
+		return -1;
+	}
+	
+	public static void main(String args[])
+	{
+	}
+}
